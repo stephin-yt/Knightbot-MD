8a13672cf82d733c4e9631127f838dd8a4748803
Stephin <93263203+stephin-yt@users.noreply.github.com>
2025-01-30 17:00:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "HISER",
+  botOwner: 'Stephin',//Your name
+  ownerNumber: '917034219328', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
